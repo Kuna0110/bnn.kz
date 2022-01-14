b14daed1f6a64470f031edd8e1528904c71d0620
Your NameKuna0110 <sardar.kuna@gmail.com>
2022-01-14 17:52:55 +0600

    14.01.2022

--- js/main.js
+++ js/main.js
@@ -1,26 +1,28 @@
 // Зафиксировать меню
 $(function () {
-    menu_top = $('.box-nav').offset().top;
+    menu_top = $('.box-nav-inner').offset().top;
     $(window).scroll(function () {
         if ($(window).scrollTop() > menu_top) {
-            if ($('.box-nav').css('position') != 'fixed') {
-                $('.box-nav').css('position', 'fixed');
-                $('.box-nav').css('top', '0');
-                $('.box-nav').css('background', '#fff');
-                $('.box-nav').css('border-bottom', '1px solid #6FCF97');
+            if ($('.box-nav-inner').css('position') != 'fixed') {
+                $('.box-nav-inner').css('position', 'fixed');
+                $('.box-nav-inner').css('top', '0');
+                $('.box-nav-inner').css('background', '#fff');
+                $('.box-nav-inner').css('border-bottom', '1px solid #6FCF97');
 
                 //logo
                 $('.logo').css('font-size', '1.5rem');
+                $('.logo').css('padding', '5px');
             }
         } else {
-            if ($('.box-nav').css('position') == 'fixed') {
-                $('.box-nav').css('position', '');
-                $('.box-nav').css('top', '');
-                $('.box-nav').css('background', '');
-                $('.box-nav').css('border-bottom', '');
+            if ($('.box-nav-inner').css('position') == 'fixed') {
+                $('.box-nav-inner').css('position', '');
+                $('.box-nav-inner').css('top', '');
+                $('.box-nav-inner').css('background', '');
+                $('.box-nav-inner').css('border-bottom', '');
 
                 //logo
                 $('.logo').css('font-size', '');
+                $('.logo').css('padding', '');
             }
         }
     });
